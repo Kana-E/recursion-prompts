305b3b7a64b449a0f275a1857e5e3f6680e63429
Kana-E <kana.eguchi@yahoo.com>
2020-04-24 11:32:40 -0400

    fill in code for sumBelow

--- src/recursion.js
+++ src/recursion.js
@@ -37,16 +37,17 @@ var arraySum = function(array) {
    if(array.length === 0){
        return 0;
    }else{
-       if(Array.isArray(array[0])){
+       if(typeof array[0] === 'number'){
+        return array[0] + arraySum(array.slice(1));
+       }else if(Array.isArray(array[0])){
            if((array[0]).length === 0){
                return 0;
            }else{
                return (array[0][0]) + arraySum((array[0]).slice(1));
            }
        }
-
-     return array[0] + arraySum(array.slice(1));
    }
+   return array[0] + arraySum(array.slice(1));
 };
 
 // 4. Check if a number is even.
@@ -63,9 +64,17 @@ var isEven = function(n) {
 
 // 5. Sum all integers below a given integer.
 // sumBelow(10); // 45
-// sumBelow(7); // 21
+// sumBelow(7); // 21  654321
 var sumBelow = function(n) {
-};
+    if(n === 0){
+        return 0;
+    }else if(n < 0){
+        return (n + 1) + sumBelow(n + 1);
+    }else if(n > 0){
+        return (n - 1) + sumBelow(n - 1);
+      }
+    }
+
 
 // 6. Get the integers within a range (x, y).
 // range(2,9); // [3,4,5,6,7,8]
