6c34499bd1cbe6ba6b6990adf31d66f4bcb0a10f
Kana-E <kana.eguchi@yahoo.com>
2020-04-24 17:20:02 -0400

    fill in code for power of two!

--- src/recursion.js
+++ src/recursion.js
@@ -115,6 +115,14 @@ var exponent = function(base, exp) {
 // powerOfTwo(16); // true
 // powerOfTwo(10); // false
 var powerOfTwo = function(n) {
+  if(n / 2 === 2 || n / 2 === 1 || n === 1){
+      return true;
+  }else if(n / 2 < 2){
+      return false;
+  }else if(n / 2 > 2){
+      return powerOfTwo(n / 2);
+  }
+
 };
 
 // 9. Write a function that reverses a string.
