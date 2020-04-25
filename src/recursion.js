7b26cc2fa10e29077466f4cb1dca65ca0064f1fe
Kana-E <kana.eguchi@yahoo.com>
2020-04-25 09:13:07 -0400

    fill in gcd code

--- src/recursion.js
+++ src/recursion.js
@@ -213,6 +213,15 @@ var divide = function(x, y) {
 // http://www.cse.wustl.edu/~kjg/cse131/Notes/Recursion/recursion.html
 // https://www.khanacademy.org/computing/computer-science/cryptography/modarithmetic/a/the-euclidean-algorithm
 var gcd = function(x, y) {
+    if(x === 0 || y === 0){
+        return 0 || x;
+    }else if(x < 0 || y < 0){
+        return null;
+    }else if(x < y){
+        return gcd(y, y - x);
+    }else{
+        return gcd(y, x - y);
+    }
 };
 
 // 15. Write a function that compares each character of two strings and returns true if
