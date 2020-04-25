0e657e809c46a3f13d215c4ccf57670e68087da4
Kana-E <kana.eguchi@yahoo.com>
2020-04-24 22:27:21 -0400

    fill in multiply code (1error remaining)

--- src/recursion.js
+++ src/recursion.js
@@ -168,6 +168,24 @@ var modulo = function(x, y) {
 // 12. Write a function that multiplies two numbers without using the * operator or
 // Math methods.
 var multiply = function(x, y) {
+  if(y === 1){
+      return x;
+  }else if(x === 1){
+      return y;
+  }else if(y === -1 && x > 0){
+      return -x;
+  }else if (y === -1 && x < 0){
+      return x;
+  }else if(x === 0 || y === 0){
+      return 0;
+  }else if(x > 0 && y > 0 || x < 0 && y > 0){
+      return x + multiply(x, y - 1);
+  }else if(x > 0 && y < 0){
+      return -x + multiply(x, y + 1);
+  }else if(x < 0 && y < 0){
+      return x + multiply(x, y + 1);
+  }
+
 };
 
 // 13. Write a function that divides two numbers without using the / operator or
